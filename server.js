d5c1f80bb0efcde5bc2f7d4854049cbdf9e59141
Stoica Alexandru <alex_stoicajr@yahoo.co.uk>
2012-08-10 15:56:30 -0400

    first commit frm cloud9

---
+++ server.js
@@ -0,0 +1,33 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app) ;
+var fs = require('fs') ;
+
+app.listen(process.env.PORT || 8001);
+
+function handler (req, res) 
+{
+    fs.readFile('index.html',
+    function (err, data) 
+    {
+        if (err) 
+        {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+    
+        res.writeHead(200, {'Content-Type': 'text/html', "Content-Length": data.length});
+        res.end(data);
+    } );
+}
+
+io.sockets.on('connection', function (socket) {
+// echo the message
+socket.on ( 'custom' , function (data) {
+    console.warn ( "+++++" + data ) ;
+    socket.emit ( 'custom' , data ) ;
+});
+socket.on('message', function (data) {
+console.info(data);
+socket.send("[ECHO] "+data);
+});
+});
