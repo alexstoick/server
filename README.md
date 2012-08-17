89d85ac117ac235105a789c865e9ba7439418e5f
Stoica Alexandru <alex_stoicajr@yahoo.co.uk>
2012-08-18 01:31:51 +0300

    now adding rooms and keeping them serverside. also new prj version and readme.

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 server for the upcoming conquiz app.
-
-
-the prj folder is a submodule -- of the alexstoick/conquiz repo.
--- server.js
+++ server.js
@@ -5,156 +5,170 @@ var url = require ('url');
 
 app.listen(process.env.PORT || 8001);
 
+var rooms = 0 ;
+
 function handler (req, res)
 {
-    var pathname = url.parse(req.url).pathname;
+	var pathname = url.parse(req.url).pathname;
 
-    var file = "" ;
-    var type="" ;
-    switch ( pathname )
-    {
-        case '/': file = 'prj/project.html' ; type = "text/html" ; break ;
-        case '/room2.html': file = 'room2.html'; type = "text/html" ; break ;
-        case '/index.html': file = 'index.html'; type = "text/html" ; break ;
-        case '/project' : file = 'prj/project.html'; type = "text/html" ; break ;
-        case '/uijs.js': file = 'prj/uijs.js' ; type = "text/javascript" ; break ;
-        case '/publicApi.js': file = 'prj/publicApi.js' ; type = "text/javascript" ; break ;
-        case '/raphael-min.js': file = 'prj/raphael-min.js' ; type = "text/javascript" ; break ;
-        case '/project.css': file = 'prj/project.css'; type ="text/css" ;break ;
-        case '/javaUtils.js': file = 'prj/javaUtils.js'; type = "text/javascript" ; break ;
-        case '/login.js': file = 'prj/login.js'; type = "text/javascript" ; break ;
-        case '/map.js': file = 'prj/map.js'; type = "text/javascript" ; break ;
-        case '/room.js': file = 'prj/room.js'; type = "text/javascript" ; break ;
+	var file = "" ;
+	var type="" ;
+	switch ( pathname )
+	{
+		case '/': file = 'prj/project.html' ; type = "text/html" ; break ;
+		case '/room2.html': file = 'room2.html'; type = "text/html" ; break ;
+		case '/index.html': file = 'index.html'; type = "text/html" ; break ;
+		case '/project' : file = 'prj/project.html'; type = "text/html" ; break ;
+		case '/uijs.js': file = 'prj/uijs.js' ; type = "text/javascript" ; break ;
+		case '/publicApi.js': file = 'prj/publicApi.js' ; type = "text/javascript" ; break ;
+		case '/raphael-min.js': file = 'prj/raphael-min.js' ; type = "text/javascript" ; break ;
+		case '/project.css': file = 'prj/project.css'; type ="text/css" ;break ;
+		case '/javaUtils.js': file = 'prj/javaUtils.js'; type = "text/javascript" ; break ;
+		case '/login.js': file = 'prj/login.js'; type = "text/javascript" ; break ;
+		case '/map.js': file = 'prj/map.js'; type = "text/javascript" ; break ;
+		case '/room.js': file = 'prj/room.js'; type = "text/javascript" ; break ;
+	}
 
-    }
+	console.log("Request for " + pathname + " received. ===> file:" + file + " type: " + type ) ;
 
-    console.log("Request for " + pathname + " received. ===> file:" + file + " type: " + type ) ;
+	fs.readFile( file ,
+	function (err, data)
+	{
+		if (err)
+		{
+			res.writeHead(500);
+			return res.end('Error loading ' + file + "~~" );
+		}
 
-    fs.readFile( file ,
-    function (err, data)
-    {
-        if (err)
-        {
-            res.writeHead(500);
-            return res.end('Error loading ' + file + "~~" );
-        }
-
-        res.writeHead(200, {'Content-Type': type, "Content-Length": data.length});
-        res.end(data);
-    } );
+		res.writeHead(200, {'Content-Type': type, "Content-Length": data.length});
+		res.end(data);
+	} );
 
 }
 
 
 io.sockets.on ( 'connection' ,
-    function (socket) {
+	function (socket) {
 
-        socket.on ( 'joinRoom' , function (room , username ) { joinRoom ( socket, room , username ) ; } ) ;
+		socket.on ( 'noRoom' , function ( ) { socket.join ( 'noRoom' ) ; } ) ;
 
-        socket.on( 'disconnect', function() { console.log ( "disconnected ~@!" ) ; disconnected ( socket ) ; } ) ;
+		socket.on ( 'joinRoom' , function (room , username ) { joinRoom ( socket, room , username ) ; } ) ;
 
-        socket.on ( 'message' , function ( data ) { receivedMessage ( socket,  data ) ; } );
+		socket.on( 'disconnect', function() { console.log ( "disconnected ~@!" ) ; disconnected ( socket ) ; } ) ;
 
-        socket.on ( 'updateMap' , function ( id , username ) { updateMap ( socket, id , username ) ; } );
+		socket.on ( 'message' , function ( data ) { receivedMessage ( socket,  data ) ; } );
 
-        socket.on ( 'answer' , function ( room , username , answer , time ) { sendAnswerToUsers ( socket , room , username , answer , time ) ; } ) ;
+		socket.on ( 'updateMap' , function ( id , username ) { updateMap ( socket, id , username ) ; } );
 
-        socket.on ( 'requestUsers' , function ( room ) { sendUsersForRoom ( socket, room ) ; } ) ;
-    } ) ;
+		socket.on ( 'answer' , function ( room , username , answer , time ) { sendAnswerToUsers ( socket , room , username , answer , time ) ; } ) ;
 
+		socket.on ( 'requestUsers' , function ( room ) { sendUsersForRoom ( socket, room ) ; } ) ;
+
+		socket.on( 'newRoom' , function ( room ) { sendNewRoomToUsers ( socket , room ) ; } ) ;
+
+		socket.on ( 'requestRoomNumber' , function ( ) { socket.emit ( 'roomNumber' , rooms) ; } ) ;
+	} ) ;
+
+
+function sendNewRoomToUsers ( socket , room )
+{
+	socket.broadcast.to('noRoom').emit ( 'addRoom' , room ) ;
+	++rooms ;
+}
 
 function getProperty ( socket , propertyName )
 {
-    var prop ;
-    socket.get ( propertyName , function ( err , property)
-                                {
-                                    console.log ( propertyName + " :: " + property ) ;
-                                    prop =  property ;
-                                }) ;
-    return prop ;
+	var prop ;
+	socket.get ( propertyName , function ( err , property)
+								{
+									console.log ( propertyName + " :: " + property ) ;
+									prop =  property ;
+								}) ;
+	return prop ;
 }
 
 function sendUsersForRoom ( socket , room )
 {
-    var conn = getClientsFromRoom( room ) ;
-    socket.emit ( 'usersForSpecificRoom' , conn , room ) ;
+	var conn = getClientsFromRoom( room ) ;
+	socket.emit ( 'usersForSpecificRoom' , conn , room ) ;
 }
 
 function sendAnswerToUsers ( socket ,  room , username , answer , time )
 {
-    socket.broadcast.to(room).send ( ">> " + username + " chose: " + answer + " in:" + time ) ;
-    socket.send ( ">> " + username + " chose: " + answer + " in:" + time ) ;
+	socket.broadcast.to(room).send ( ">> " + username + " chose: " + answer + " in:" + time ) ;
+	socket.send ( ">> " + username + " chose: " + answer + " in:" + time ) ;
 
 }
 
 function getClientsFromRoom ( room )
 {
-    var connectedArray = [] ;
-    var i ;
+	var connectedArray = [] ;
+	var i ;
 
-    console.log ( "roomId " + room ) ;
+	console.log ( "roomId " + room ) ;
 
-    for ( i = 0 ; i < io.sockets.clients(room).length ; ++ i )
-    {
-        var currSocket = io.sockets.clients(room)[i];
-        var user = getProperty( currSocket , 'username' ) ;
-        connectedArray.push ( user ) ;
-    }
-    console.log ( "||||||||||||||||||||||||||" + connectedArray ) ;
-    return connectedArray ;
+	for ( i = 0 ; i < io.sockets.clients(room).length ; ++ i )
+	{
+		var currSocket = io.sockets.clients(room)[i];
+		var user = getProperty( currSocket , 'username' ) ;
+		connectedArray.push ( user ) ;
+	}
+	console.log ( "||||||||||||||||||||||||||" + connectedArray ) ;
+	return connectedArray ;
 
 }
 
 function joinRoom ( socket , room , username )
 {
 
-    socket.set ( 'room' , room ) ;
-    socket.set ( 'username' , username ) ;
+	socket.leave ( 'noRoom' ) ;
+	socket.set ( 'room' , room ) ;
+	socket.set ( 'username' , username ) ;
 
-    console.log ( "username:" + username + "    room:" + room ) ;
+	console.log ( "username:" + username + "    room:" + room ) ;
 
-    socket.join ( room ) ;
-    socket.send ( ">>> joined " + room ) ;
+	socket.join ( room ) ;
+	socket.send ( ">>> joined " + room ) ;
 
-    var connectedArray = getClientsFromRoom ( room ) ;
+	var connectedArray = getClientsFromRoom ( room ) ;
 
-    socket.emit ( 'usersUpdate' , connectedArray ) ;
-    socket.broadcast.to(room).emit ( 'usersUpdate' , connectedArray ) ;
+	socket.emit ( 'usersUpdate' , connectedArray ) ;
+	socket.broadcast.to(room).emit ( 'usersUpdate' , connectedArray ) ;
 }
 
 
 function disconnected ( socket )
 {
-    var user = getProperty( socket , 'username' ) ;
-    var room = getProperty( socket , 'room' ) ;
+	var user = getProperty( socket , 'username' ) ;
+	var room = getProperty( socket , 'room' ) ;
 
-    console.log ( "Disconnected " + user + " from room:" + room ) ;
-    socket.broadcast.to(room).emit ( 'userDisconnected' , user ) ;
+	console.log ( "Disconnected " + user + " from room:" + room ) ;
+	socket.broadcast.to(room).emit ( 'userDisconnected' , user ) ;
 }
 
 function receivedMessage ( socket , data )
 {
-    console.log("Client data: " + data);
+	console.log("Client data: " + data);
 
-    // lookup room and broadcast to that room
-    var room ;
-    room = getProperty( socket , 'room' ) ;
-    console.log ( "Client room:" + room ) ;
-    socket.broadcast.to(room).emit( 'message' , data );
+	// lookup room and broadcast to that room
+	var room ;
+	room = getProperty( socket , 'room' ) ;
+	console.log ( "Client room:" + room ) ;
+	socket.broadcast.to(room).emit( 'message' , data );
 }
 
 
 function updateMap ( socket , id , username  )
 {
-    var room ;
-    console.log ( "~~update map" ) ;
-    room = getProperty( socket , 'room' ) ;
-    console.log ( "UPDATING " + room + " zone " + id + " by user:" + username ) ;
-    socket.broadcast.to(room).emit ( 'mapUpdate' , id , username ) ;
-    socket.emit ( 'mapUpdate' , id , username ) ;
+	var room ;
+	console.log ( "~~update map" ) ;
+	room = getProperty( socket , 'room' ) ;
+	console.log ( "UPDATING " + room + " zone " + id + " by user:" + username ) ;
+	socket.broadcast.to(room).emit ( 'mapUpdate' , id , username ) ;
+	socket.emit ( 'mapUpdate' , id , username ) ;
 
-    socket.broadcast.to(room).emit ( 'showQuestion' ) ;
-    socket.emit ( 'showQuestion' ) ;
+	socket.broadcast.to(room).emit ( 'showQuestion' ) ;
+	socket.emit ( 'showQuestion' ) ;
 }
 
 
